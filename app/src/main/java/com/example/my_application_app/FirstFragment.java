2428ebd569eecbf93807ff30e7b8e4b4caf797e7
hyebin0411 <4444ssome@gmail.com>
2022-05-09 17:37:52 +0900

    ViewPager home

---
+++ app/src/main/java/com/example/my_application_app/FirstFragment.java
@@ -0,0 +1,34 @@
+package com.example.my_application_app;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class FirstFragment extends Fragment {
+    private String title;
+    private int page;
+
+    public static FirstFragment newInstance(int page, String title){
+        FirstFragment fragment = new FirstFragment();
+        Bundle args = new Bundle();
+        args.putInt("someInt", page);
+        args.putString("someTitle", title);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        page = getArguments().getInt("someInt", 0);
+        title = getArguments().getString("someTitle");
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.fragment_first);
+    }
+}
--- app/src/main/java/com/example/my_application_app/MainActivity.java
+++ app/src/main/java/com/example/my_application_app/MainActivity.java
@@ -9,17 +9,63 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import me.relex.circleindicator.CircleIndicator;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    FragmentPagerAdapter adapterViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ViewPager vpPager = (ViewPager) findViewById(R.id.vpPager);
+        adapterViewPager = new MyPagerAdapter(getSupportFragmentManager());
+        vpPager.setAdapter(adapterViewPager);
+
+        CircleIndicator indicator = (CircleIndicator) findViewById(R.id.indicator);
+        indicator.setViewPager(vpPager);
+
         Intent intent1 = new Intent(getApplicationContext(), menu_first.class);
+    }
+
+    public static class MyPagerAdapter extends FragmentPagerAdapter{
+        private static int NUM_ITEMS = 3;
+
+        public MyPagerAdapter(FragmentManager fragmentManager) {
+            super(fragmentManager);
+        }
+
+        @Override
+        public int getCount(){
+            return NUM_ITEMS;
+        }
+
+        @Override
+        public Fragment getItem(int position){
+            switch (position) {
+                case 0:
+                    return FirstFragment.newInstance(0, "Pager # 1");
+                case 1:
+                    return SecondFragmnet.newInstance(1, "Pager # 2");
+                case 2:
+                    return ThirdFragment.newInstance(2, "Pager # 3");
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position){
+            return "Page " + position;
+        }
     }
 
 
